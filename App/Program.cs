3b90f8e6e06f086c1eb0a411451f69465a334e00
Taha <Taha@gmail.com>
2023-09-19 11:27:43 +0300

    Multi-Dimensional Array

--- App/Program.cs
+++ App/Program.cs
@@ -6,43 +6,45 @@ public class Program
 {
     public static void Main()
     {
-        Trainee[] sdf = new Trainee[]
+        Trainee[] Points = new Trainee[]
         {
-            new Trainee { Id =1, Name="Ahmed"},
-            new Trainee { Id =2, Name="Hany"}
+            new Trainee() {Id=1, Name ="Hany"},
+            new Trainee() {Id=2, Name ="Ahmed"},
         };
-        Trainee[] dotNet = new Trainee[]
+        Trainee[] Expensives = new Trainee[]
         {
-            new Trainee { Id =1, Name="Doaa"},
-            new Trainee { Id =2, Name="Saja"},
-            new Trainee { Id =3, Name="Gamal"}
+            new Trainee() {Id=3, Name ="Doaa"},
+            new Trainee() {Id=4, Name ="Sally"},
         };
-        Trainee[][] Tracks = new Trainee[2][] { dotNet, sdf};
 
+       
 
-        foreach (Trainee[] row in Tracks)
+        Trainee[,] Teams = new Trainee[2, 2] 
         {
-            foreach(Trainee t in row)
-                Console.WriteLine(t);
-
-            Console.WriteLine();
-            Console.WriteLine();
-        }
-
-        /*
-        for(int i =0; i < Tracks.Length; i++)
-        {
-            for(int j =0; j < Tracks[i].Length; j++)
-                Console.WriteLine(Tracks[i][j].ToString());
-
-            Console.WriteLine();
-            Console.WriteLine();
-        }
-        */
+            {
+                new Trainee() {Id=1, Name ="Hany"},
+                new Trainee() {Id=2, Name ="Ahmed"},
+            },
+            {
+             new Trainee() {Id=3, Name ="Doaa"},
+            new Trainee() {Id=4, Name ="Sally"},
+            }
+        };
 
 
-
-
+        Console.WriteLine(Teams[1, 0]);
         Console.WriteLine();
+        Console.WriteLine();
+        foreach(Trainee t in Teams)
+            Console.WriteLine(t);
+
+
+        for (int i = 0; i < Teams.GetLength(0); i++)
+        {
+            for (int j = 0; j < Teams.GetLength(1); j++)
+                Console.WriteLine(Teams[i, j]);
+        }
+
+
     }
 }
