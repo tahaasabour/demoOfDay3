3b068686463f29e17fbffe507d5bf1c4caaf3d62
Taha <Taha@gmail.com>
2023-09-19 10:17:08 +0300

    Basics Of Arrays

--- App/Program.cs
+++ App/Program.cs
@@ -6,8 +6,26 @@ public class Program
 {
     public static void Main()
     {
-        Trainee t = new Trainee() { Id= 1, Name = "Hany"};
-        Console.WriteLine(t.ToString());
+        Trainee[] arr = new Trainee[]
+        {
+            new Trainee{ Id=1, Name="Hany"}, 
+            new Trainee{ Id=2,Name="Doaa"}
+        };
 
+
+        foreach(Trainee obj in arr)
+        {
+            Console.WriteLine(obj);
+        }
+
+        /*
+        for(int i =0; i< arr.Length; i++)
+        {
+            Console.WriteLine(arr[i].ToString());
+        }
+        */
+
+
+        //Console.WriteLine(arr[1].ToString());
     }
 }
