2f5203d7bfe2e0bd75dab3b545cf07e96abd69cc
Taha <Taha@gmail.com>
2023-09-19 11:10:22 +0300

    Array Of Arrays Basics

--- App/Program.cs
+++ App/Program.cs
@@ -6,43 +6,43 @@ public class Program
 {
     public static void Main()
     {
-        int[] arr = new int[] { 10, 20 };
-        int[] arr2 = new int[arr.Length];
+        Trainee[] sdf = new Trainee[]
+        {
+            new Trainee { Id =1, Name="Ahmed"},
+            new Trainee { Id =2, Name="Hany"}
+        };
+        Trainee[] dotNet = new Trainee[]
+        {
+            new Trainee { Id =1, Name="Doaa"},
+            new Trainee { Id =2, Name="Saja"},
+            new Trainee { Id =3, Name="Gamal"}
+        };
+        Trainee[][] Tracks = new Trainee[2][] { dotNet, sdf};
 
 
-       // Array.Resize(ref arr2, arr2.Length + 1);
+        foreach (Trainee[] row in Tracks)
+        {
+            foreach(Trainee t in row)
+                Console.WriteLine(t);
 
-        arr2[2] = 90;
-
-        foreach (int j in arr2)
-            Console.WriteLine(j);
-
-
-        //Array.Copy(arr, arr2, arr.Length);
-
-
-        //int[] arr3 = new int[arr2.Length + 1];
-        //Array.Copy(arr2, arr3, arr2.Length);
-
-
-        // arr2 = arr3;
-
-        //arr2[2] = 50;
-
-        //foreach (int j in arr2)
-        //    Console.WriteLine(j);
+            Console.WriteLine();
+            Console.WriteLine();
+        }
 
         /*
-        for(int i =0; i < arr.Length; i++)
-            arr2[i] = arr[i];
-          */
-        /*
-        foreach(int j in arr)
-            Console.WriteLine(j);
+        for(int i =0; i < Tracks.Length; i++)
+        {
+            for(int j =0; j < Tracks[i].Length; j++)
+                Console.WriteLine(Tracks[i][j].ToString());
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
         */
 
+
+
+
         Console.WriteLine();
-
-
     }
 }
