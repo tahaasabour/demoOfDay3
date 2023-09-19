aa63f4cc0e5cd8c2334c168476a226ac7e26571b
Taha <Taha@gmail.com>
2023-09-19 09:52:10 +0300

    Initial Commit

---
+++ App/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+using Models;
+
+public class Program
+{
+    public static void Main()
+    {
+        Trainee t = new Trainee() { Id= 1, Name = "Hany"};
+        Console.WriteLine(t.ToString());
+
+    }
+}
