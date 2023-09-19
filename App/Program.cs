a550f8d3e569803995a54cf9f03ec8c2361ec9f3
Taha <Taha@gmail.com>
2023-09-19 10:56:06 +0300

    Array Static Methods , Copy, ReSize

--- App/Program.cs
+++ App/Program.cs
@@ -6,26 +6,43 @@ public class Program
 {
     public static void Main()
     {
-        Trainee[] arr = new Trainee[]
-        {
-            new Trainee{ Id=1, Name="Hany"}, 
-            new Trainee{ Id=2,Name="Doaa"}
-        };
+        int[] arr = new int[] { 10, 20 };
+        int[] arr2 = new int[arr.Length];
 
 
-        foreach(Trainee obj in arr)
-        {
-            Console.WriteLine(obj);
-        }
+       // Array.Resize(ref arr2, arr2.Length + 1);
+
+        arr2[2] = 90;
+
+        foreach (int j in arr2)
+            Console.WriteLine(j);
+
+
+        //Array.Copy(arr, arr2, arr.Length);
+
+
+        //int[] arr3 = new int[arr2.Length + 1];
+        //Array.Copy(arr2, arr3, arr2.Length);
+
+
+        // arr2 = arr3;
+
+        //arr2[2] = 50;
+
+        //foreach (int j in arr2)
+        //    Console.WriteLine(j);
 
         /*
-        for(int i =0; i< arr.Length; i++)
-        {
-            Console.WriteLine(arr[i].ToString());
-        }
+        for(int i =0; i < arr.Length; i++)
+            arr2[i] = arr[i];
+          */
+        /*
+        foreach(int j in arr)
+            Console.WriteLine(j);
         */
 
+        Console.WriteLine();
 
-        //Console.WriteLine(arr[1].ToString());
+
     }
 }
