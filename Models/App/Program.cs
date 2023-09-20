a9cce3363fced096ff3d7d8c8c86ec365af507e6
sohayb ahmed <engsohayb97@gmail.com>
2023-09-20 11:50:01 +0300

    Day3_Task_Sohayb

---
+++ Models/App/Program.cs
@@ -0,0 +1,12 @@
+﻿using Models;
+using System.Collections;
+public class TraineeComparer : IComparer
+{
+    public int Compare(object? x, object? y)
+    {
+        Trainee left = x as Trainee;
+        Trainee right = y as Trainee;
+
+        return left.Id.CompareTo(right.Id);
+    }
+}
---
+++ Models/Models/Trainee.cs
@@ -0,0 +1,19 @@
+﻿namespace Models
+{
+    public class Trainee : IComparable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public override string ToString()
+        {
+            return $"{Id}, {Name}";
+        }
+        int IComparable.CompareTo(object obj)
+        {
+            Trainee T = obj as Trainee;
+
+            return Id.CompareTo(T.Id);
+
+        }
+    }
+}
